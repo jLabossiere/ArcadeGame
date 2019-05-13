0309c003cf9a8d62704ca79f0d3761ffc5195762
Julien Labossiere <jdhlabossiere@gmail.com>
2019-05-13 11:33:04 -0500

    near complete gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -17,3 +17,42 @@ gulp.task('style', () => {
 		.pipe(cleanCSS({ compatibility: 'ie8' }))
 		.pipe(gulp.dest('./dest/css'));
 });
+
+gulp.task('copy-images', () => {
+	return gulp
+		.src('./src/images/**')
+		.pipe(imagemin())
+		.pipe(gulp.dest('./dest/images'));
+});
+
+gulp.task('copy-js', () => {
+	return gulp
+		.src([
+			'./src/js/resources.js',
+			'./src/js/engine.js',
+			'./src/js/app.js',
+			'./src/js/music.js'
+		])
+		.pipe(
+			babel({
+				presets: ['@babel/env']
+			})
+		)
+		.pipe(concat('all.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('./dest/js'));
+});
+
+gulp.task('copy-HTML', () => {
+	return gulp.src('./src/index.html').pipe(gulp.dest('./dest'));
+});
+
+gulp.task('watch', () => {
+	gulp.watch('./src/js/**', gulp.series('copy-js'));
+	gulp.watch('./src/css/**/*.css', gulp.series('style'));
+	gulp.watch('./src/index.html', gulp.series('copy-HTML'));
+});
+
+gulp.task('all', () => {
+	gulp.parallel('style', 'copy-images', 'copy-js', 'copy-HTML', 'watch');
+});
